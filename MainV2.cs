f1f933602019460472d3edadba92272329c1b93c
Volodymyr Zibarov <gogan419@gmail.com>
2024-11-09 12:18:16 +0200

    increase RC channels override rate from 20 to 64 Hz
    Signed-off-by: Volodymyr Zibarov <gogan419@gmail.com>

--- MainV2.cs
+++ MainV2.cs
@@ -2230,7 +2230,7 @@ namespace MissionPlanner
         /// </summary>
         private async void joysticksend()
         {
-            float rate = 50; // 1000 / 50 = 20 hz
+            float rate = 10;
             int count = 0;
 
             DateTime lastratechange = DateTime.Now;
@@ -2427,7 +2427,7 @@ namespace MissionPlanner
                         }
                     }
 
-                    await Task.Delay(40).ConfigureAwait(false);
+                    await Task.Delay(5).ConfigureAwait(false);
                 }
                 catch
                 {
